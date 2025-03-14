21c3c79bfd5e83faaaa48c1a25184a8e312290ab
Karen Florez <florezkaren76@gmail.com>
2025-03-14 18:23:48 -0300

    última actualización del proyecto

--- app/app.py
+++ app/app.py
@@ -7,15 +7,15 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
-    app = Flask(__name__) # Se crea una instancia de la app Flask
+    app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
     migrate.init_app(app, db)
-    
-    from app.routes import hallazgos, incidentes # Importa las rutas de los módulos
-    app.register_blueprint(hallazgos.bp)
-    app.register_blueprint(incidentes.bp)
 
-    return app # Devuelve la aplicación configurada
-    
+    with app.app_context():
+        from app.routes import hallazgos, incidentes
+        app.register_blueprint(hallazgos.bp)
+        app.register_blueprint(incidentes.bp)
+    return app
+app = create_app()
